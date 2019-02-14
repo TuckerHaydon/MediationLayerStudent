0a18393577763dcf46bea3baea80de6f289d58a2
tuckerhaydon <tuckerhaydon@gmail.com>
2019-02-14 17:39:40 -0600

    Fixed up map2sfc example

--- examples/map2sfc_example.cc
+++ examples/map2sfc_example.cc
@@ -7,7 +7,7 @@
 #include "map2d.h"
 #include "gui2d.h"
 #include "graph.h"
-#include "a_star.h"
+#include "a_star2d.h"
 #include "dijkstra.h"
 #include "timer.h"
 
@@ -54,65 +54,63 @@ int main(int argc, char** argv) {
 
   OccupancyGrid2D occupancy_grid;
   occupancy_grid.LoadFromMap(map, SAMPLE_DELTA, SAFETY_BOUND);
-  // Graph graph(occupancy_grid);
+  Graph2D graph = occupancy_grid.AsGraph();
 
-  // const size_t data_size = 2*sizeof(int);
-  // const double start[2] = {9,1}, end[2] = {9,9};
-  // const int start_int[2] = {(int)(start[0]/SAMPLE_DELTA),(int)(start[1]/SAMPLE_DELTA)};
-  // const int end_int[2] = {(int)(end[0]/SAMPLE_DELTA),(int)(end[1]/SAMPLE_DELTA)};
+  const size_t data_size = 2*sizeof(int);
+  const double start[2] = {9,1}, end[2] = {9,9};
+  const int start_int[2] = {(int)(start[0]/SAMPLE_DELTA),(int)(start[1]/SAMPLE_DELTA)};
+  const int end_int[2] = {(int)(end[0]/SAMPLE_DELTA),(int)(end[1]/SAMPLE_DELTA)};
 
-  // Node start_node, end_node;
-  // start_node.SetData(reinterpret_cast<const uint8_t*>(start_int), data_size);
-  // end_node.SetData(reinterpret_cast<const uint8_t*>(end_int), data_size);
+  const auto start_node = std::make_shared<Node2D>(Eigen::Vector2d(start_int[0], start_int[1]));
+  const auto end_node = std::make_shared<Node2D>(Eigen::Vector2d(end_int[0], end_int[1]));
+  const AStar2D::Path a_star_solution = AStar2D().Run(graph, start_node, end_node);
 
-  // const std::vector<Node> node_path = AStar().Run(graph, start_node, end_node);
+  {
+    std::vector<Point2D> path;
+    path.reserve(a_star_solution.nodes.size());
+    std::for_each(
+      a_star_solution.nodes.begin(),
+      a_star_solution.nodes.end(),
+      [&](const auto& node){
+        path.emplace_back(
+            // Rows, cols to x, y
+            node->Data().y(),
+            node->Data().x());
+      });
 
-  // {
-  //   std::vector<Point2D> path;
-  //   path.reserve(node_path.size());
-  //   // Rows, cols to x, y
-  //   std::for_each(
-  //     node_path.begin(),
-  //     node_path.end(),
-  //     [&](const Node& node){
-  //       path.emplace_back(
-  //           ((int*)node.Data())[1],
-  //           ((int*)node.Data())[0]);
-  //     });
+    Gui2D gui;
+    gui.LoadOccupancyGrid(&occupancy_grid);
+    gui.LoadPath(path);
+    gui.Display();
+  }
 
-  //   Gui2D gui;
-  //   gui.LoadOccupancyGrid(&occupancy_grid);
-  //   gui.LoadPath(path);
-  //   gui.Display();
-  // }
+  {
+    std::vector<Point2D> path;
+    path.reserve(a_star_solution.nodes.size());
+    std::for_each(
+      a_star_solution.nodes.begin(),
+      a_star_solution.nodes.end(),
+      [&](const auto& node){
+        path.emplace_back(
+            // Rows, cols to x, y
+            node->Data().y() * SAMPLE_DELTA,
+            node->Data().x() * SAMPLE_DELTA);
+      });
 
+    SafeFlightCorridor2D sfc2d;
+    sfc2d.SetMap(map);
+    auto timer = Timer("SFC timer finished");
+    timer.Start();
+    std::vector<Polygon> sfc = sfc2d.Run(path);
+    timer.Stop();
+    timer.Print();
 
-  // {
-  //   std::vector<Point2D> path;
-  //   path.reserve(node_path.size());
-  //   // Rows, cols to x, y
-  //   std::for_each(
-  //       node_path.begin(),
-  //       node_path.end(),
-  //       [&](const Node& node){
-  //         path.emplace_back(
-  //             ((int*)node.Data())[1] * SAMPLE_DELTA,
-  //             ((int*)node.Data())[0] * SAMPLE_DELTA);
-  //       });
-
-  //   SafeFlightCorridor2D sfc2d;
-  //   sfc2d.SetMap(map);
-  //   auto timer = Timer("SFC timer finished");
-  //   timer.Start();
-  //   std::vector<Polygon> sfc = sfc2d.Run(path);
-  //   timer.Stop();
-
-  //   Gui2D gui;
-  //   gui.LoadMap(map);
-  //   gui.LoadSafeFlightCorridor(sfc);
-  //   gui.LoadPath(path);
-  //   gui.Display();
-  // }
+    Gui2D gui;
+    gui.LoadMap(map);
+    gui.LoadSafeFlightCorridor(sfc);
+    gui.LoadPath(path);
+    gui.Display();
+  }
 
   return EXIT_SUCCESS;
 }
