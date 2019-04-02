19a40c2d58ed7761ca93f9df085f85ec1a51e1e7
tuckerhaydon <tuckerhaydon@gmail.com>
2019-04-02 16:56:29 -0500

    Added ros messages

--- src/ros/node/trajectory_publisher_node.h
+++ src/ros/node/trajectory_publisher_node.h
@@ -3,13 +3,13 @@
 #pragma once
 
 #include <ros/ros.h>
-#include <std_msgs/String.h>
 #include <memory>
 #include <string>
 #include <functional>
 
 #include "trajectory.h"
 #include "publisher_guard.h"
+#include "mg_msgs/PVAYStampedTrajectory.h"
 
 namespace mediation_layer {
   // Trajectory publisher acts as an adapter between the ROS ecosystem and the
@@ -20,14 +20,14 @@ namespace mediation_layer {
     private:
       // A publisher guard ensures that the Publish() function may be called in
       // a thread-safe manner
-      std::shared_ptr<PublisherGuard<std_msgs::String>> publisher_guard_;
+      std::shared_ptr<PublisherGuard<mg_msgs::PVAYStampedTrajectory>> publisher_guard_;
       
     public:
       // Constructor.
       TrajectoryPublisherNode(const std::string& topic);
 
       // Publishes the message
-      void Publish(const Trajectory<T>& msg);
+      void Publish(const Trajectory<T>& trajectory);
   };
 
   //  ******************
@@ -36,17 +36,48 @@ namespace mediation_layer {
   template <size_t T>
   inline TrajectoryPublisherNode<T>::TrajectoryPublisherNode(
       const std::string& topic) {
-    this->publisher_guard_ = std::make_shared<PublisherGuard<std_msgs::String>>(topic);
+    this->publisher_guard_ 
+      = std::make_shared<PublisherGuard<mg_msgs::PVAYStampedTrajectory>>(topic);
   }
 
   template <size_t T>
-  inline void TrajectoryPublisherNode<T>::Publish(const Trajectory<T>& msg) {
-    std_msgs::String m;
-    m.data = "";
-    this->publisher_guard_->Publish(m);
+  inline void TrajectoryPublisherNode<T>::Publish(const Trajectory<T>& trajectory) {
+    mg_msgs::PVAYStampedTrajectory msg;
+    for(size_t idx = 0; idx < trajectory.Size(); ++idx) {
+      mg_msgs::PVAYStamped instant;
+      if(T == 2) {
+        instant.pos.x = trajectory.Position(idx).x();
+        instant.pos.y = trajectory.Position(idx).y();
+        instant.vel.linear.x = trajectory.Velocity(idx).x();
+        instant.vel.linear.y = trajectory.Velocity(idx).y();
+        instant.acc.linear.x = trajectory.Acceleration(idx).x();
+        instant.acc.linear.y = trajectory.Acceleration(idx).y();
+        instant.yaw = trajectory.Yaw(idx);
+        instant.header.stamp.sec = std::floor(trajectory.Time(idx));
+        instant.header.stamp.nsec 
+          = (trajectory.Time(idx) - std::floor(trajectory.Time(idx))) * 1e9;
+      } else
+      if(T == 3) {
+        instant.pos.x = trajectory.Position(idx).x();
+        instant.pos.y = trajectory.Position(idx).y();
+        instant.pos.z = trajectory.Position(idx).z();
+        instant.vel.linear.x = trajectory.Velocity(idx).x();
+        instant.vel.linear.y = trajectory.Velocity(idx).y();
+        instant.vel.linear.z = trajectory.Velocity(idx).z();
+        instant.acc.linear.x = trajectory.Acceleration(idx).x();
+        instant.acc.linear.y = trajectory.Acceleration(idx).y();
+        instant.acc.linear.z = trajectory.Acceleration(idx).z();
+        instant.yaw = trajectory.Yaw(idx);
+        instant.header.stamp.sec = std::floor(trajectory.Time(idx));
+        instant.header.stamp.nsec 
+          = (trajectory.Time(idx) - std::floor(trajectory.Time(idx))) * 1e9;
+      }
+
+      msg.trajectory.push_back(instant);
+    }
+    this->publisher_guard_->Publish(msg);
   }
 
   using TrajectoryPublisherNode2D = TrajectoryPublisherNode<2>;
   using TrajectoryPublisherNode3D = TrajectoryPublisherNode<3>;
-
-};
+}
--- src/ros/node/trajectory_subscriber_node.h
+++ src/ros/node/trajectory_subscriber_node.h
@@ -3,13 +3,13 @@
 #pragma once
 
 #include <ros/ros.h>
-#include <std_msgs/String.h>
 #include <memory>
 #include <string>
 #include <functional>
 
 #include "trajectory_warden.h"
 #include "trajectory.h"
+#include "mg_msgs/PVAYStampedTrajectory.h"
 
 namespace mediation_layer {
   // Trajectory subscriber acts as an adapter between the ROS ecosystem and the
@@ -23,7 +23,7 @@ namespace mediation_layer {
       ros::Subscriber subscriber_;
       std::string key_;
 
-      void SubscriberCallback(const std_msgs::String& msg);
+      void SubscriberCallback(const mg_msgs::PVAYStampedTrajectory& msg);
       
     public:
       // Constructor.
@@ -54,11 +54,43 @@ namespace mediation_layer {
   }
 
   template <size_t T>
-  void TrajectorySubscriberNode<T>::SubscriberCallback(const std_msgs::String& msg) {
-    Trajectory<T> trajectory;
-    this->warden_->Write(this->key_, trajectory);
+  void TrajectorySubscriberNode<T>::SubscriberCallback(const mg_msgs::PVAYStampedTrajectory& msg) {
+      // Required data structure. Formatted as follows:
+      //   [ pos(T), vel(T), acc(T), yaw(1), time(1)]
+      std::vector<
+        Eigen::Vector<double, 3*T + 2>, 
+        Eigen::aligned_allocator<Eigen::Vector<double, 3*T + 2>>> data;
+    for(const mg_msgs::PVAYStamped& instant: msg.trajectory) {
+      Eigen::Vector<double, 3*T + 2> local_instant;
+      if(T == 2) {
+        local_instant(0) = instant.pos.x;
+        local_instant(1) = instant.pos.y;
+        local_instant(2) = instant.vel.linear.x;
+        local_instant(3) = instant.vel.linear.y;
+        local_instant(4) = instant.acc.linear.x;
+        local_instant(5) = instant.acc.linear.y;
+        local_instant(6) = instant.yaw;
+        local_instant(7) = instant.header.stamp.sec + instant.header.stamp.nsec / 1e9;
+      } else
+      if (T == 3) {
+        local_instant(0) = instant.pos.x;
+        local_instant(1) = instant.pos.y;
+        local_instant(2) = instant.pos.z;
+        local_instant(3) = instant.vel.linear.x;
+        local_instant(4) = instant.vel.linear.y;
+        local_instant(5) = instant.vel.linear.z;
+        local_instant(6) = instant.acc.linear.x;
+        local_instant(7) = instant.acc.linear.y;
+        local_instant(8) = instant.acc.linear.z;
+        local_instant(9) = instant.yaw;
+        local_instant(10) = instant.header.stamp.sec + instant.header.stamp.nsec / 1e9;
+      }
+      data.push_back(local_instant);
+    }
+
+    this->warden_->Write(this->key_, Trajectory<T>(data));
   }
 
   using TrajectorySubscriberNode2D = TrajectorySubscriberNode<2>;
   using TrajectorySubscriberNode3D = TrajectorySubscriberNode<3>;
-};
+}
