c27a6765115d79c05338be8f8f4ac56e3f9d9462
tuckerhaydon <tuckerhaydon@gmail.com>
2019-04-23 16:52:04 -0500

    fixed potential bug in mediation_layer

--- src/mediation_layer/mediation_layer.h
+++ src/mediation_layer/mediation_layer.h
@@ -15,11 +15,17 @@ namespace mediation_layer {
   // trajetories might cause quads to fly into each other, walls, or other
   // obstacles in the environment. 
   //
-  // TODO: Modify trajectories to prevent quads from flying into them
-  // TODO: Change hard-coded key 'phoenix'
+  // TODO: Modify trajectories to prevent quads from flying into obstacles
   class MediationLayer {
     private:
       volatile std::atomic_bool ok_{true};
+
+      // Transfers data associated with key from trajectory_warden_in to
+      // trajectory_warden_out
+      void TransferData(
+          const std::string& key,
+          std::shared_ptr<TrajectoryWarden> trajectory_warden_in,
+          std::shared_ptr<TrajectoryWarden> trajectory_warden_out);
 
     public:
       MediationLayer() {}
@@ -32,6 +38,7 @@ namespace mediation_layer {
           std::shared_ptr<TrajectoryWarden> trajectory_warden_out,
           std::shared_ptr<QuadStateWarden> state_warden);
 
+      // Stop this thread and all sub-threads
       void Stop();
 
   };
@@ -39,17 +46,56 @@ namespace mediation_layer {
   //  ******************
   //  * IMPLEMENTATION *
   //  ******************
+  inline void MediationLayer::TransferData(
+      const std::string& key,
+      std::shared_ptr<TrajectoryWarden> trajectory_warden_in,
+      std::shared_ptr<TrajectoryWarden> trajectory_warden_out) {
+
+      while(true == this->ok_) {
+        Trajectory trajectory;
+        trajectory_warden_in->Await(key, trajectory);
+        trajectory_warden_out->Write(key, trajectory);
+      }
+  }
+
   inline void MediationLayer::Run(
       std::shared_ptr<TrajectoryWarden> trajectory_warden_in,
       std::shared_ptr<TrajectoryWarden> trajectory_warden_out,
       std::shared_ptr<QuadStateWarden> state_warden) {
 
-    while(this->ok_) {
-      const std::string key = "phoenix";
-      Trajectory trajectory;
-      trajectory_warden_in->Await(key, trajectory);
-      trajectory_warden_out->Write(key, trajectory);
+    // Local thread pool 
+    std::vector<std::thread> thread_pool;
+
+    // Get all registered trajectories
+    const std::set<std::string> state_keys = state_warden->Keys();
+
+    // Assign a thread to await changes in each trajectory
+    for(const std::string& key: state_keys) {
+      thread_pool.push_back(
+          std::move(
+            std::thread([&](){
+              this->TransferData(key, trajectory_warden_in, trajectory_warden_out);
+              })));
     }
+
+    // Wait for this thread to receive a stop command
+    std::thread kill_thread(
+        [&, this]() {
+          while(true) {
+            if(false == this->ok_) {
+              break;
+            } else {
+              std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+          }
+        });
+
+    kill_thread.join();
+
+    // Wait for thread pool to terminate
+    for(std::thread& t: thread_pool) {
+      t.join();
+    } 
   }
 
   inline void MediationLayer::Stop() {
